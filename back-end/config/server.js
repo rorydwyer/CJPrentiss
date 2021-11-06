3f18894a96da9641509475fe350f16eecdee6367
Rory <rory.dwy@gmail.com>
2021-11-05 17:42:06 -0700

    Init

---
+++ back-end/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '11a191964b75b75f0491fd3e89dbc81c'),
+    },
+  },
+});

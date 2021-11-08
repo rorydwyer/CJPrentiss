540bec1aac463260c268aa32eec466cfc6632d11
Rory <rory.dwy@gmail.com>
2021-11-07 18:28:45 -0800

    Animation text

--- front-end/src/components/Hero.js
+++ front-end/src/components/Hero.js
@@ -4,14 +4,13 @@ import anime from "animejs/lib/anime.es.js";
 
 const Hero = () => {
   useEffect(() => {
-    let textWrapper = document.querySelector("#intro-text");
+    let textWrapper = document.querySelector("#hero-text");
     textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
     anime.timeline().add({
-      targets: "#intro-text .letter",
+      targets: "#hero-text .letter",
       translateY: [100, 0],
       translateZ: 0,
-      // opacity: [0, 1],
       easing: "easeOutExpo",
       duration: 2000,
       delay: (el, i) => 1800 + 35 * i,
@@ -31,7 +30,7 @@ const Hero = () => {
         height: "100vh",
       }}
     >
-      <div id="intro-text" className="text-9xl text-white fixed -bottom-2 left-5">
+      <div id="hero-text" className="text-9xl text-white fixed -bottom-2 left-5">
         Christa Prentiss
       </div>
     </ParallaxBanner>
--- front-end/src/components/Intro.js
+++ front-end/src/components/Intro.js
@@ -1,11 +1,12 @@
 import { Parallax } from "react-scroll-parallax";
-
 const Intro = ({ intro }) => {
   return (
     <Parallax y={[-20, 20]}>
       <div className="container mx-auto flex flex-col justify-center md:pl-24 md:pr-52 h-screen px-4 my-32 text-gray-700">
         <p className="italic text-xl font-serif mb-8">- hello!</p>
-        <h2 className="text-6xl leading-relaxed font-light">{intro}</h2>
+        <h2 id="intro-text" className="text-6xl leading-relaxed font-light">
+          {intro}
+        </h2>
       </div>
     </Parallax>
   );

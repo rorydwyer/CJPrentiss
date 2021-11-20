151cb2a12467b495fc7cf43aafbe47c3423214d7
Rory <rory.dwy@gmail.com>
2021-11-20 11:07:57 -0800

    Fix footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,13 @@
 const Footer = () => {
   return (
-    <footer className="flex container justify-between mx-auto mt-4 mb-6 z-50 transform translate-x-0">
-      <span>
-        Made with &#10084; by{" "}
+    <footer className="flex container flex-wrap justify-around md:justify-between mx-auto mt-4 mb-6 z-50 transform translate-x-0">
+      <span className="px-6">
+        Made with &#10084; &nbsp; by &nbsp;
         <a href="https://rory-dwyer.com" className="hover:underline" target="_blank" rel="noreferrer">
           Rory Dwyer
         </a>
       </span>
-      <span>&copy; 2021 Christa Prentiss, All Rights Reserved.</span>
+      <span className="px-6">&copy; 2021 Christa Prentiss, All Rights Reserved.</span>
     </footer>
   );
 };
